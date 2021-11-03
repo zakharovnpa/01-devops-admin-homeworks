0b7a6464d4cf524584d1cdd57edf9d0d999a0fd6
zakharovnpa <89311498+zakharovnpa@users.noreply.github.com>
2021-11-03 11:52:06 +0400

    Create traceroute.bash

---
+++ 03-sysadmin-08-net/traceroute.bash
@@ -0,0 +1,49 @@
+route-views>
+route-views>traceroute 128.71.38.234
+Type escape sequence to abort.
+Tracing the route to 128-71-38-234.broadband.corbina.ru (128.71.38.234)
+VRF info: (vrf in name/id, vrf out name/id)
+  1 vl-51-gw.uoregon.edu (128.223.51.1) [AS 3582] 0 msec 1 msec 0 msec
+  2 vl-51-gw.uoregon.edu (128.223.51.1) [AS 3582] 1 msec 1 msec 0 msec
+  3 10.252.20.5 [AS 174] 4 msec
+    10.252.19.1 [AS 174] 0 msec
+    10.252.19.5 [AS 174] 1 msec
+  4 10.252.10.249 [AS 174] 0 msec
+    10.252.9.249 [AS 174] 1 msec 0 msec
+  5 10.252.10.246 [AS 174] 2 msec
+    10.252.9.246 [AS 174] 1 msec
+    10.252.10.246 [AS 174] 1 msec
+  6 eugn-pe2-gw.nero.net (207.98.68.226) [AS 3701] 0 msec 1 msec 0 msec
+  7 cenic--lon-100ge.cenic.net (137.164.3.102) [AS 3701] 13 msec
+    eugn-p1-gw.nero.net (207.98.64.75) [AS 3701] [MPLS: Label 380560 Exp 0] 3 msec 2 msec
+  8 eugn-pe1-gw.nero.net (207.98.64.205) [AS 3701] 1 msec
+    eugn-pe1-gw.nero.net (207.98.64.197) [AS 3701] 1 msec 1 msec
+  9 cenic--lon-100ge.cenic.net (137.164.3.102) [AS 3701] 22 msec
+    ae1-3121.edge8.Frankfurt1.level3.net (4.69.158.186) [AS 3356] 161 msec 174 msec
+ 10 EDN-SOVINTE.edge8.Frankfurt1.Level3.net (212.162.40.254) [AS 3356] 195 msec 195 msec *
+ 11 ae1-3121.edge8.Frankfurt1.level3.net (4.69.158.186) [AS 3356] 197 msec *  *
+ 12 81.211.51.102 [AS 3216] 214 msec
+    EDN-SOVINTE.edge8.Frankfurt1.Level3.net (212.162.40.254) [AS 3356] 195 msec 195 msec
+ 13 195.239.76.225 [AS 3216] 214 msec *  *
+ 14  *
+    195.239.76.227 [AS 3216] 214 msec
+    81.211.51.102 [AS 3216] 220 msec
+ 15  *
+    195.239.76.225 [AS 3216] 217 msec *
+ 16  *  *
+    195.239.76.227 [AS 3216] 214 msec
+ 17  *  *  *
+ 18  *  *  *
+ 19  *  *  *
+ 20  *  *  *
+ 21  *  *  *
+ 22  *  *  *
+ 23  *  *  *
+ 24  *  *  *
+ 25  *  *  *
+ 26  *  *  *
+ 27  *  *  *
+ 28  *  *  *
+ 29  *  *  *
+ 30  *  *  *
+route-views>
