f569f9fa607acb86a879a7250e4cb921fd600fc7
zakharovnpa <89311498+zakharovnpa@users.noreply.github.com>
2021-11-03 13:01:36 +0400

    Create netstat-st.bash

---
+++ 03-sysadmin-08-net/netstat-st.bash
@@ -0,0 +1,49 @@
+root@mx-linux:~# netstat -st
+IcmpMsg:
+    OutType3: 183
+Tcp:
+    108 active connection openings
+    4 passive connection openings
+    0 failed connection attempts
+    2 connection resets received
+    3 connections established
+    8062 segments received
+    6238 segments sent out
+    61 segments retransmitted
+    13 bad segments received
+    281 resets sent
+UdpLite:
+TcpExt:
+    58 TCP sockets finished time wait in fast timer
+    36 delayed acks sent
+    Quick ack mode was activated 49 times
+    4824 packet headers predicted
+    1044 acknowledgments not containing data payload received
+    595 predicted acknowledgments
+    16 congestion windows recovered without slow start after partial ack
+    TCPLostRetransmit: 1
+    TCPTimeouts: 16
+    TCPLossProbes: 45
+    TCPBacklogCoalesce: 6
+    TCPDSACKOldSent: 48
+    TCPDSACKRecv: 43
+    21 connections reset due to unexpected data
+    1 connections reset due to early user close
+    TCPSACKDiscard: 1
+    TCPDSACKIgnoredNoUndo: 13
+    TCPSpuriousRTOs: 1
+    TCPRcvCoalesce: 2111
+    TCPOFOQueue: 231
+    TCPChallengeACK: 13
+    TCPSYNChallenge: 13
+    TCPAutoCorking: 185
+    TCPOrigDataSent: 2007
+    TCPHystartTrainDetect: 2
+    TCPHystartTrainCwnd: 38
+    TCPKeepAlive: 248
+    TCPDelivered: 2093
+    TCPAckCompressed: 72
+    TcpTimeoutRehash: 16
+    TcpDuplicateDataRehash: 4
+    TCPDSACKRecvSegs: 42
+    TCPDSACKIgnoredDubious: 1
