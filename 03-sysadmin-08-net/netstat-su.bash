bed3468e4473d535790852bd4419bf4350f0d503
zakharovnpa <89311498+zakharovnpa@users.noreply.github.com>
2021-11-03 13:03:08 +0400

    Create netstat-su.bash

---
+++ 03-sysadmin-08-net/netstat-su.bash
@@ -0,0 +1,24 @@
+root@mx-linux:~# netstat -su
+IcmpMsg:
+    OutType3: 183
+Udp:
+    1152 packets received
+    183 packets to unknown port received
+    0 packet receive errors
+    845 packets sent
+    0 receive buffer errors
+    0 send buffer errors
+    IgnoredMulti: 108
+UdpLite:
+IpExt:
+    InMcastPkts: 57
+    OutMcastPkts: 26
+    InBcastPkts: 270
+    OutBcastPkts: 77
+    InOctets: 9478529
+    OutOctets: 952297
+    InMcastOctets: 6696
+    OutMcastOctets: 4382
+    InBcastOctets: 28947
+    OutBcastOctets: 10007
+    InNoECTPkts: 10411
