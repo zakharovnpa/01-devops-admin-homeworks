483dac9d6c82ba0327caa845778c9dbdabb15847
zakharovnpa <89311498+zakharovnpa@users.noreply.github.com>
2021-11-03 11:45:01 +0400

    Update route-views.bash

--- 03-sysadmin-08-net/route-views.bash
+++ 03-sysadmin-08-net/route-views.bash
@@ -1,3 +1,29 @@
+**********************************************************************
+
+                    RouteViews BGP Route Viewer
+                    route-views.routeviews.org
+
+ route views data is archived on http://archive.routeviews.org
+
+ This hardware is part of a grant by the NSF.
+ Please contact help@routeviews.org if you have questions, or
+ if you wish to contribute your view.
+
+ This router has views of full routing tables from several ASes.
+ The list of peers is located at http://www.routeviews.org/peers
+ in route-views.oregon-ix.net.txt
+
+ NOTE: The hardware was upgraded in August 2014.  If you are seeing
+ the error message, "no default Kerberos realm", you may want to
+ in Mac OS X add "default unset autologin" to your ~/.telnetrc
+
+ To login, use the username "rviews".
+
+ **********************************************************************
+
+User Access Verification
+
+Username: rviews
 route-views>
 route-views>sh ip route 128.71.38.234
 Routing entry for 128.71.0.0/16
