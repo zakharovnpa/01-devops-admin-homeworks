836a1e45deaa7ce20153632ca3cbd88cc8c29edd
zakharovnpa <89311498+zakharovnpa@users.noreply.github.com>
2021-11-03 18:43:18 +0400

    Update route-views.bash

--- 03-sysadmin-08-net/route-views.bash
+++ 03-sysadmin-08-net/route-views.bash
@@ -30,15 +30,3 @@ User Access Verification
 
 Username: rviews
 route-views>
-route-views>sh ip route 128.71.38.234
-Routing entry for 128.71.0.0/16
-  Known via "bgp 6447", distance 20, metric 0
-  Tag 2497, type external
-  Last update from 202.232.0.2 4d13h ago
-  Routing Descriptor Blocks:
-  * 202.232.0.2, from 202.232.0.2, 4d13h ago
-      Route metric is 0, traffic share count is 1
-      AS Hops 2
-      Route tag 2497
-      MPLS label: none
-route-views>
