9236971bab6be15bf94f57ba44699f203909d491
zakharovnpa <89311498+zakharovnpa@users.noreply.github.com>
2021-11-03 17:11:10 +0400

    Create ss-t.bash

---
+++ 03-sysadmin-08-net/ss-t.bash
@@ -0,0 +1,5 @@
+root@vagrant:~# ss -t
+State                  Recv-Q                   Send-Q                                     Local Address:Port                                     Peer Address:Port                   Process
+ESTAB                  0                        0                                              10.0.2.15:ssh                                          10.0.2.2:51628
+ESTAB                  0                        0                                              10.0.2.15:ssh                                          10.0.2.2:50789
+ESTAB                  0                        0                                              10.0.2.15:ssh                                          10.0.2.2:64425
