ba1ef2e32ae2f617af644c3d396d0976f59fe1f8
zakharovnpa <89311498+zakharovnpa@users.noreply.github.com>
2021-11-03 19:16:47 +0400

    Create ss-ua.bash

---
+++ 03-sysadmin-08-net/ss-ua.bash
@@ -0,0 +1,28 @@
+root@mx-linux:~# ss -ua
+State                   Recv-Q                  Send-Q                                                        Local Address:Port                                            Peer Address:Port
+UNCONN                  0                       0                                                                   0.0.0.0:47594                                                0.0.0.0:*
+UNCONN                  0                       0                                                                   0.0.0.0:ipp                                                  0.0.0.0:*
+UNCONN                  0                       0                                                                 127.0.0.1:836                                                  0.0.0.0:*
+UNCONN                  0                       0                                                                 127.0.0.1:8125                                                 0.0.0.0:*
+UNCONN                  0                       0                                                                   0.0.0.0:bootpc                                               0.0.0.0:*
+UNCONN                  0                       0                                                                   0.0.0.0:sunrpc                                               0.0.0.0:*
+UNCONN                  0                       0                                                              192.168.1.99:ntp                                                  0.0.0.0:*
+UNCONN                  0                       0                                                                 127.0.0.1:ntp                                                  0.0.0.0:*
+UNCONN                  0                       0                                                                   0.0.0.0:ntp                                                  0.0.0.0:*
+UNCONN                  0                       0                                                                   0.0.0.0:50301                                                0.0.0.0:*
+UNCONN                  0                       0                                                             192.168.1.255:netbios-ns                                           0.0.0.0:*
+UNCONN                  0                       0                                                              192.168.1.99:netbios-ns                                           0.0.0.0:*
+UNCONN                  0                       0                                                                   0.0.0.0:netbios-ns                                           0.0.0.0:*
+UNCONN                  0                       0                                                             192.168.1.255:netbios-dgm                                          0.0.0.0:*
+UNCONN                  0                       0                                                              192.168.1.99:netbios-dgm                                          0.0.0.0:*
+UNCONN                  0                       0                                                                   0.0.0.0:netbios-dgm                                          0.0.0.0:*
+UNCONN                  0                       0                                                                   0.0.0.0:mdns                                                 0.0.0.0:*
+UNCONN                  0                       0                                                                      [::]:48563                                                   [::]:*
+UNCONN                  0                       0                                                                      [::]:52670                                                   [::]:*
+UNCONN                  0                       0                                                                         *:1716                                                       *:*
+UNCONN                  0                       0                                                                     [::1]:8125                                                    [::]:*
+UNCONN                  0                       0                                                                      [::]:sunrpc                                                  [::]:*
+UNCONN                  0                       0                                          [fe80::38d6:bc97:eb58:f482]%eth0:ntp                                                     [::]:*
+UNCONN                  0                       0                                                                     [::1]:ntp                                                     [::]:*
+UNCONN                  0                       0                                                                      [::]:ntp                                                     [::]:*
+UNCONN                  0                       0                                                                      [::]:mdns                                                    [::]:*
