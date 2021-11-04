f302684bc5fd317104903f983861ad47d02051a5
zakharovnpa <89311498+zakharovnpa@users.noreply.github.com>
2021-11-04 10:50:30 +0400

    Create ss-s.bash

---
+++ 03-sysadmin-08-net/ss-s.bash
@@ -0,0 +1,10 @@
+root@mx-linux:~# ss -s
+Total: 538
+TCP:   42 (estab 16, closed 9, orphaned 0, timewait 9)
+
+Transport Total     IP        IPv6
+RAW       1         0         1
+UDP       20        14        6
+TCP       33        24        9
+INET      54        38        16
+FRAG      0         0         0
